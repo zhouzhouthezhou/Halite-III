f33505839aaac50fc068062040afa3df5a6a961a
zhouzhouthezhou <kylezhou2222@gmail.com>
2018-12-01 13:21:56 -0700

    initial commit

---
+++ MyBot.cpp
@@ -0,0 +1,126 @@
+#include "hlt/game.hpp"
+#include "hlt/constants.hpp"
+#include "hlt/log.hpp"
+
+#include <random>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+using namespace hlt;
+
+struct Node
+{
+	int halite;
+	Position position;
+	int id;
+	int fleet = -1;
+};
+
+struct FleetShip
+{
+	shared_ptr<ship> ship;
+	Position destination;
+	int row;
+	int state;
+	int fleet;
+};
+
+struct Fleet
+{
+	FleetShip ships[4];
+	int id;
+	vector<int> nodes;
+};
+
+vector<Node> nodes;
+vector<Fleet> fleets;
+
+int nextNode(FleetShip ship, int DIM)
+{
+	Position pos = ship.ship->pos;
+	int currentNode = (pos.x / 4) + (pos.y / 4) * (DIM / 4);
+	int[] nextNodes = { currentNode + 1, currentNode - 1, currentNode - DIM / 4, currentNode + DIM / 4 };
+	sort(nextNodes.begin(), nextNodes.end, bool func(int a, int b) { return nodes[a].halite > nodes[b].halite });
+	for (int i : nextNodes)
+	{
+		if (nodes[nextNodes].fleet == -1)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main(int argc, char* argv[]) {
+	unsigned int rng_seed;
+	if (argc > 1) {
+		rng_seed = static_cast<unsigned int>(stoul(argv[1]));
+	}
+	else {
+		rng_seed = static_cast<unsigned int>(time(nullptr));
+	}
+	mt19937 rng(rng_seed);
+
+	Game game;
+
+	int DIM = game.game_map.cells.size();
+	for (unsigned int i = 0; i < DIM; i += 4)
+	{
+		for (unsigned int j = 0; j < DIM; j += 4)
+		{
+			nodes.push_back(Node{ 0, Position(i, j), (i / 4 + j / 4 * DIM / 4) };
+		}
+	}
+	int totHal = 0;
+	for (unsigned int i = 0; i < DIM; ++i)
+	{
+		for (unsigned int j = 0; j < DIM; ++j)
+		{
+			totHal += game.game_map.at(Position(i, j))->halite;
+			nodes[i / 4 + j / 4 * DIM / 4].halite += game.game_map.at(Position(i, j))->halite;
+		}
+	}
+
+	// At this point "game" variable is populated with initial map data.
+	// This is a good place to do computationally expensive start-up pre-processing.
+	// As soon as you call "ready" function below, the 2 second per turn timer will start.
+
+	game.ready("MyCppBot");
+
+	log::log("Successfully created bot! My Player ID is " + to_string(game.my_id) + ". Bot rng seed is " + to_string(rng_seed) + ".");
+
+	for (;;) {
+		game.update_frame();
+		shared_ptr<Player> me = game.me;
+		unique_ptr<GameMap>& game_map = game.game_map;
+
+		vector<Command> command_queue;
+
+		for (const auto& ship_iterator : me->ships) {
+			shared_ptr<Ship> ship = ship_iterator.second;
+
+			if (game_map->at(ship)->halite < constants::MAX_HALITE / 10 || ship->is_full()) {
+				Direction random_direction = ALL_CARDINALS[rng() % 4];
+				command_queue.push_back(ship->move(random_direction));
+			}
+			else {
+				command_queue.push_back(ship->stay_still());
+			}
+		}
+
+		if (
+			game.turn_number <= 200 &&
+			me->halite >= constants::SHIP_COST &&
+			!game_map->at(me->shipyard)->is_occupied())
+		{
+			command_queue.push_back(me->shipyard->spawn());
+		}
+
+		if (!game.end_turn(command_queue)) {
+			break;
+		}
+	}
+
+	return 0;
+}
